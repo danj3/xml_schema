b91adf882f470197283ba587fded326176f16134
Dan Janowski <danj@3skel.com>
2023-10-19 22:50:35 +0200

    prep for hex package

--- mix.exs
+++ mix.exs
@@ -8,8 +8,35 @@ defmodule XmlSchema.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths()
+      elixirc_paths: elixirc_paths(),
+      package: package(),
+
+      # Docs
+      name: "XmlSchema",
+      source_url: "https://github.com/danj3/xml_schema",
+      docs: [
+        main: "readme",
+        extras: [ "README.md" ]
+      ]
+
     ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [ ~r{.*~$} ],
+      description: description(),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "github" => "https://github.com/danj3/xml_schema"
+      }
+    ]
+  end
+
+  defp description do
+    """
+    Parse and generate XML using DSL extension of Ecto.Schema.
+    """
   end
 
   defp elixirc_paths do
@@ -36,7 +63,8 @@ defmodule XmlSchema.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ecto, "~> 3.10"},
       {:erlsom, "~> 1.5"},
-      {:xml_builder, "~> 2.2"}
+      {:xml_builder, "~> 2.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 end
